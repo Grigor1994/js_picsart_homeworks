9f7b0d86d1bcc1c43e793d6d1624af889cf6d3b8
Grigor1994 <grigor.avetisyan@hotmail.com>
2021-02-19 12:10:47 +0400

    initial commit

--- nodejs_env/dotenv.js
+++ nodejs_env/dotenv.js
@@ -6,14 +6,18 @@ function config() {
     let data = fileData.split('\n');
     for (let i = 0; i < data.length; i++) {
         const [key, value] = data[i].split("=");
-        if (validate(key))
-            continue;
-        process.env[key] = value;
+        if (validate(key)) continue;
+        if (value === undefined) continue;
+        process.env[key] = parse(value);
     }
 }
 
 function validate(key) {
     return key.match(REGEXP);
+}
+
+function parse(value) {
+    return value.replace(/['"]/g, '');
 }
 
 module.exports = {config: config};
