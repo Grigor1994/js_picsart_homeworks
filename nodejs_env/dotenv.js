adffb9f77111a7d566c934a08f2516b8a968a6c2
Grigor1994 <grigor.avetisyan@hotmail.com>
2021-02-19 11:48:07 +0400

    initial commit

--- nodejs_env/dotenv.js
+++ nodejs_env/dotenv.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
+const REGEXP = /([a-zA-z0-9$_]*[=]+)/
 
 function config() {
     let fileData = fs.readFileSync('.././.env', 'utf-8');
     let data = fileData.split('\n');
-    let uniqueData = new Set(data);
-    const newArray = [...uniqueData];
-    for (let i = 0; i < newArray.length; i++) {
-        const [key, value] = newArray[i].split("=");
-        if(key.startsWith('=')|| key.startsWith('#'))
+    for (let i = 0; i < data.length; i++) {
+        const [key, value] = data[i].split("=");
+        if (validate(key))
             continue;
         process.env[key] = value;
     }
+}
+
+function validate(key) {
+    return key.match(REGEXP);
 }
 
 module.exports = {config: config};
--- nodejs_env/index.js
+++ nodejs_env/index.js
@@ -1,4 +1,5 @@
 require('./dotenv').config();
 
-console.log(process.env.PING);
 console.log(process.env.NAME);
+console.log(process.env.PING);
+
